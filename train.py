14fc18f049aef807d34245a08b2925b0058dcf05
Avinash Ranganath <nash911@gmail.com>
2023-05-07 23:51:48 +0200

    First commit

---
+++ train.py
@@ -0,0 +1,85 @@
+from __future__ import unicode_literals, print_function, division
+# from io import open
+# import unicodedata
+# import string
+# import re
+import random
+
+import numpy as np
+import os
+import torch
+import argparse
+# import json
+
+from datetime import datetime
+from shutil import rmtree
+
+# import time
+# import math
+
+import matplotlib.pyplot as plt
+# import matplotlib.ticker as ticker
+
+from ae import EncoderRNN, AttnDecoderRNN, trainIters
+
+from utils import prepareData
+
+def main(args):
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+
+    # Create a timestamp directory to save model, parameter and log files
+    training_dir = \
+        ('training/' + str(datetime.now().date()) + '_' +
+         str(datetime.now().hour).zfill(2) + '-' + str(datetime.now().minute).zfill(2) +
+         '/')
+
+    # Delete if a directory with the same name already exists
+    if os.path.exists(training_dir):
+        rmtree(training_dir)
+
+    # Create empty directories for saving model, parameter and log files
+    os.makedirs(training_dir)
+    os.makedirs(training_dir + 'plots')
+    os.makedirs(training_dir + 'models')
+    os.makedirs(training_dir + 'params')
+
+    device = torch.device(f"cuda:{args.gpu}" if torch.cuda.is_available() else "cpu")
+
+    input_lang, output_lang, pairs = prepareData('eng', 'fra', True)
+    print(random.choice(pairs))
+
+    encoder1 = EncoderRNN(input_lang.n_words, args.hidden_size, device).to(device)
+    attn_decoder1 = AttnDecoderRNN(args.hidden_size, output_lang.n_words, args.max_length,
+                                   device, dropout_p=0.1).to(device)
+
+    trainIters(encoder1, attn_decoder1, pairs, n_iters=args.n_iters, device=device,
+               max_length=args.max_length, teacher_ratio=args.teacher_ratio,
+               print_every=100, plot_show=args.plot, path=training_dir)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Expert Policy Distillation')
+    parser.add_argument('--batch-size', type=int, default=32,
+                        help='input batch size for training (default: 32)')
+    parser.add_argument('--hidden_size', type=int, default=256,
+                        help='hidden representation size (default: 256)')
+    parser.add_argument('--max_length', type=int, default=10,
+                        help='maximum sequence length (default: 10)')
+    parser.add_argument('--teacher_ratio', type=float, default=0.5,
+                        help='teacher forcing ratio for decoder input (default: 0.5)')
+    parser.add_argument('--lr', type=float, default=0.01,
+                        help='learning rate for optimizer (default: 0.01)')
+    parser.add_argument('--n_iters', type=int, default=75000,
+                        help='number of training iterations (default: 75000)')
+    parser.add_argument('--data-path', type=str, default=None,
+                        help='Path to the data file (default: None)')
+    parser.add_argument('--seed', type=int, default=1, help='random seed (default: 1)')
+    parser.add_argument('--gpu', type=int, default=None, help='GPU (default: None)')
+    parser.add_argument('--plot', type=bool, default=False,
+                        action=argparse.BooleanOptionalAction,
+                        help='plot learning curve (default: False)')
+    args = parser.parse_args()
+
+    main(args)
---
+++ utils.py
@@ -0,0 +1,199 @@
+from __future__ import unicode_literals, print_function, division
+import numpy as np
+from io import open
+import unicodedata
+# import string
+import re
+import random
+
+import time
+import math
+
+import matplotlib.pyplot as plt
+# import matplotlib.ticker as ticker
+#
+
+import torch
+
+from lang import Lang
+
+# plt.switch_backend('agg')
+
+SOS_token = 0
+EOS_token = 1
+
+MAX_LENGTH = 10
+
+eng_prefixes = (
+    "i am ", "i m ",
+    "he is", "he s ",
+    "she is", "she s ",
+    "you are", "you re ",
+    "we are", "we re ",
+    "they are", "they re "
+)
+
+
+def unicodeToAscii(s):
+    # Turn a Unicode string to plain ASCII, thanks to
+    # https://stackoverflow.com/a/518232/2809427
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+
+def normalizeString(s):
+    # Lowercase, trim, and remove non-letter characters
+    s = unicodeToAscii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    return s
+
+
+def readLangs(lang1, lang2, reverse=False):
+    print("Reading lines...")
+
+    # Read the file and split into lines
+    lines = open('data/%s-%s.txt' % (lang1, lang2), encoding='utf-8').\
+        read().strip().split('\n')
+
+    # Split every line into pairs and normalize
+    pairs = [[normalizeString(s) for s in l.split('\t')[:2]] for l in lines]
+
+    # Reverse pairs, make Lang instances
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        input_lang = Lang(lang2)
+        output_lang = Lang(lang1)
+    else:
+        input_lang = Lang(lang1)
+        output_lang = Lang(lang2)
+
+    return input_lang, output_lang, pairs
+
+
+def filterPair(p):
+    return len(p[0].split(' ')) < MAX_LENGTH and \
+        len(p[1].split(' ')) < MAX_LENGTH and \
+        p[1].startswith(eng_prefixes)
+
+
+def filterPairs(pairs):
+    return [pair for pair in pairs if filterPair(pair)]
+
+
+def prepareData(lang1, lang2, reverse=False):
+    input_lang, output_lang, pairs = readLangs(lang1, lang2, reverse)
+    print("Read %s sentence pairs" % len(pairs))
+    pairs = filterPairs(pairs)
+    print("Trimmed to %s sentence pairs" % len(pairs))
+    print("Counting words...")
+    for pair in pairs:
+        input_lang.addSentence(pair[0])
+        output_lang.addSentence(pair[1])
+    print("Counted words:")
+    print(input_lang.name, input_lang.n_words)
+    print(output_lang.name, output_lang.n_words)
+    return input_lang, output_lang, pairs
+
+
+input_lang, output_lang, pairs = prepareData('eng', 'fra', True)
+print(random.choice(pairs))
+
+
+def indexesFromSentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')]
+
+
+def tensorFromSentence(lang, sentence, device):
+    indexes = indexesFromSentence(lang, sentence)
+    indexes.append(EOS_token)
+    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+
+
+def tensorsFromPair(pair, device):
+    input_tensor = tensorFromSentence(input_lang, pair[0], device)
+    target_tensor = tensorFromSentence(output_lang, pair[1], device)
+    return (input_tensor, target_tensor)
+
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
+
+
+def plot_loss(axs, train_loss, plot_freq=1, show=False, save=True, path=None):
+    # Training (NLL) Loss plot
+    x = (np.arange(len(train_loss)) * plot_freq).tolist()
+    axs.clear()
+    axs.plot(x, train_loss, color='red',
+             label='Train Loss')
+    # if len(test_loss) > 0:
+    #     axs.plot(list(range(len(test_loss))), test_loss, color='blue', label='Test Loss')
+    axs.set(title='Learning Curves')
+    axs.set(ylabel='NLLL')
+    axs.set(xlabel='Iterations')
+    axs.legend(loc='upper right')
+
+    if save:
+        plt.savefig(path + "plots/learning_curves.png")
+
+    if show:
+        plt.show(block=False)
+        plt.pause(0.01)
+
+
+def evaluate(encoder, decoder, sentence, device, max_length=MAX_LENGTH):
+    with torch.no_grad():
+        input_tensor = tensorFromSentence(input_lang, sentence)
+        input_length = input_tensor.size()[0]
+        encoder_hidden = encoder.initHidden()
+
+        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+
+        for ei in range(input_length):
+            encoder_output, encoder_hidden = encoder(input_tensor[ei], encoder_hidden)
+            encoder_outputs[ei] += encoder_output[0, 0]
+
+        decoder_input = torch.tensor([[SOS_token]], device=device)  # SOS
+
+        decoder_hidden = encoder_hidden
+
+        decoded_words = []
+        decoder_attentions = torch.zeros(max_length, max_length)
+
+        for di in range(max_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            decoder_attentions[di] = decoder_attention.data
+            topv, topi = decoder_output.data.topk(1)
+            if topi.item() == EOS_token:
+                decoded_words.append('<EOS>')
+                break
+            else:
+                decoded_words.append(output_lang.index2word[topi.item()])
+
+            decoder_input = topi.squeeze().detach()
+
+        return decoded_words, decoder_attentions[:di + 1]
+
+
+def evaluateRandomly(encoder, decoder, n=10):
+    for i in range(n):
+        pair = random.choice(pairs)
+        print('>', pair[0])
+        print('=', pair[1])
+        output_words, attentions = evaluate(encoder, decoder, pair[0])
+        output_sentence = ' '.join(output_words)
+        print('<', output_sentence)
+        print('')
